3a699be8901f8b661b0b10b6e34699bcb91b5717
Kaptan_Singh <kaptanpal123098@gmail.com>
2023-04-06 07:20:19 +0530

    error of null is resolved

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,44 +1,113 @@
-import React, { Component } from 'react';
-import './Sidebar.css';
-import { HashLink as Link } from 'react-router-hash-link';
-import { FiExternalLink } from 'react-icons/fi';
-import logo from '../images/kp.jpg'
+import React, { Component } from "react";
+import "./Sidebar.css";
+import { HashLink as Link } from "react-router-hash-link";
+// import { FiExternalLink } from "react-icons/fi";
+import logo from "../images/kp.jpg";
 
 class Sidebar extends Component {
-    render() {
+  render() {
+    return (
+      <div className="sidebar">
+        <div style={{ color: "black", fontWeight: "bold" }} className="tagtop">
+          {" "}
+          # programmer_life{" "}
+        </div>
+        <div style={{ color: "black", fontWeight: "bold" }} className="">
+          {" "}
+          # hello_world{" "}
+        </div>
+        <div style={{ color: "black", fontWeight: "bold" }} className="">
+          {" "}
+          # coding{" "}
+        </div>
+        <h1>
+          <Link smooth to="/#start" className="h1_links">
+            Kaptan Singh
+          </Link>
+        </h1>
 
-        return (
-            <div className="sidebar">
-                <div style={{ color: 'black', fontWeight: 'bold' }} className="tagtop"> # programmer_life </div>
-                <div style={{ color: 'black', fontWeight: 'bold' }} className=""> # hello_world </div>
-                <div style={{ color: 'black', fontWeight: 'bold' }} className=""> # coding </div>
-                <h1><Link smooth to="/#start" className="h1_links">Kaptan Singh</Link></h1>
+        <img src={logo} alt="" />
+        <p style={{ color: "black", fontWeight: "bold" }} className="gmail">
+          {" "}
+            <a
+            href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJftvBffjPmxWxvfHWRHWnhTbpQgPxBxvkcnPpFpFqbhrXMCHXSJTsCXCVLsQzgRcVLpnPL"
+            rel="opener noreferrer"
+            target="_blank"
+            className="fa fa-envelope"
+          >{null}</a>{" "}
+          kaptan.singh5084@gmail.com{" "}
+        </p>
 
-                <img src={logo} alt="description of image"/>
-                <p style={{ color: 'black', fontWeight: 'bold' }} className="gmail"><a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJftvBffjPmxWxvfHWRHWnhTbpQgPxBxvkcnPpFpFqbhrXMCHXSJTsCXCVLsQzgRcVLpnPL" rel="opener noreferrer" target="_blank" className="fa fa-envelope"></a> kaptan.singh5084@gmail.com </p>
+        <ul className="sidebar-nav">
+          <li className="sidebar-nav-items">
+            <Link smooth to="/#about" className="links">
+              About
+            </Link>
+          </li>
+          <li className="sidebar-nav-items">
+            <Link smooth to="/#education" className="links">
+              Education
+            </Link>
+          </li>
+          <li className="sidebar-nav-items">
+            <Link smooth to="/#interest" className="links">
+              Interest
+            </Link>
+          </li>
+          {/* <li className="sidebar-nav-items"><a href="http://www.bloggingpeek.com" target="_blank" rel="opener noreferrer" className="links"> Blog<FiExternalLink/></a></li> */}
+        </ul>
 
-                <ul className="sidebar-nav">
-                    <li className="sidebar-nav-items"><Link smooth to="/#about" className="links">About</Link></li>
-                    <li className="sidebar-nav-items"><Link smooth to="/#education" className="links" >Education</Link></li>
-                    <li className="sidebar-nav-items"><Link smooth to="/#interest" className="links">Interest</Link></li>
-                    {/* <li className="sidebar-nav-items"><a href="http://www.bloggingpeek.com" target="_blank" rel="opener noreferrer" className="links"> Blog<FiExternalLink/></a></li> */}
-                </ul>
+        <div className="flip-card-back">
+          <ul className="sidebar-nav">
+            <li className="sidebar-nav-icons">
+              {" "}
+              <a
+                href="https://github.com/kaptan0428"
+                rel="opener noreferrer"
+                target="_blank"
+                className="fa fas fa-github fa-lg"
+              >{null}</a>
+            </li>
+            <li className="sidebar-nav-icons">
+              <a
+                href="https://www.linkedin.com/in/kaptan-singh-kp"
+                rel="opener noreferrer"
+                target="_blank"
+                className="fa fas fa-linkedin fa-lg"
+              >{null}</a>
+            </li>
 
-                <div className="flip-card-back">
-                    <ul className="sidebar-nav">
-                        <li className="sidebar-nav-icons"> <a href="https://github.com/kaptan0428" rel="opener noreferrer" target="_blank" className="fa fas fa-github fa-lg"></a></li>
-                        <li className="sidebar-nav-icons"><a href="https://www.linkedin.com/in/kaptan-singh-kp" rel="opener noreferrer" target="_blank" className="fa fas fa-linkedin fa-lg"></a></li>
+            <li className="sidebar-nav-icons">
+              <a
+                href="https://codeforces.com/profile/KaptanSingh"
+                rel="opener noreferrer"
+                target="_blank"
+                className="fa fas fa-code fa-lg"
+              >{null}</a>
+            </li>
+            <li className="sidebar-nav-icons">
+              <a
+                href="https://www.codechef.com/users/kaptan_singh"
+                rel="opener noreferrer"
+                target="_blank"
+                className="fa fas fa-code fa-lg"
+              >{null}</a>
+            </li>
 
-                        <li className="sidebar-nav-icons"><a href="https://codeforces.com/profile/KaptanSingh" rel="opener noreferrer" target="_blank" className="fa fas fa-code fa-lg"></a></li>
-                        <li className="sidebar-nav-icons"><a href="https://www.codechef.com/users/kaptan_singh" rel="opener noreferrer" target="_blank" className="fa fas fa-code fa-lg"></a></li>
-                        
-                        <li className="sidebar-nav-icons"> <a href="mailto:kaptan.singh5084@gmail.com" rel="opener noreferrer" target="_blank" className="fa fas fa-envelope fa-lg"></a></li>
-                    </ul>
-                </div>
-
-            </div>
-        )
-    }
+            <li className="sidebar-nav-icons">
+              {" "}
+              <a
+                href="mailto:kaptan.singh5084@gmail.com"
+                rel="opener noreferrer"
+                target="_blank"
+                className="fa fas fa-envelope fa-lg"
+              >{null}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Sidebar
+export default Sidebar;
